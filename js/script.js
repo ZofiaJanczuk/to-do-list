d30a0afaa4b962192bc9fc9960be2e11d434919d
Zofia Janczuk <janczukzosia@gmail.com>
2023-08-01 13:57:10 +0200

    Functions init and render added, CSS changes.

---
+++ js/script.js
@@ -0,0 +1,31 @@
+{
+
+    const tasks = [
+        {
+            content: "zjeść obiad",
+        }, 
+        {
+            content: "siłownia",
+        },
+    ];
+
+    const render = () => {
+        let htmlString = "";
+
+        for(const task of tasks) {
+            htmlString +=`
+            <li>
+                ${task.content};
+            </li>
+            `;
+
+            document.querySelector(".js-tasks").innerHTML = htmlString;
+        }
+    };
+
+    const init = () => {
+        render();
+    };
+
+    init();
+}
